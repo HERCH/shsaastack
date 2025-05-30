7e3e9453de556c1b3220e9b9b0bb96e1d16b3aa5
Rene Hernández <rhernandez@softherch.com>
2025-05-29 21:22:50 -0600

    feat(Settings): Add BindSection method to ISettings interface

--- src/Common/Configuration/ISettings.cs
+++ src/Common/Configuration/ISettings.cs
@@ -12,4 +12,12 @@ public interface ISettings
     public double GetNumber(string key, double? defaultValue = null);
 
     public string GetString(string key, string? defaultValue = null);
+    
+    /// <summary>
+    /// Binds a configuration section identified by the key to the provided instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the instance to bind to.</typeparam>
+    /// <param name="sectionKey">The key of the configuration section.</param>
+    /// <param name="instanceToBind">The instance to bind the configuration values to.</param>
+    void BindSection<T>(string sectionKey, T instanceToBind) where T : class;
 }
