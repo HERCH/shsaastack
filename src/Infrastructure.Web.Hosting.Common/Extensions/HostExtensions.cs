08071e4cb6304d347145718b93f01bd07c502b22
Rene Hernández <rhernandez@softherch.com>
2025-05-31 08:59:25 -0600

    chore(DI): Ensure RabbitMQ infrastructure services are registered

--- src/Infrastructure.Web.Hosting.Common/Extensions/HostExtensions.cs
+++ src/Infrastructure.Web.Hosting.Common/Extensions/HostExtensions.cs
@@ -54,14 +54,13 @@ using Infrastructure.External.Persistence.Azure.ApplicationServices;
 using Amazon.XRay.Recorder.Core;
 using Amazon.XRay.Recorder.Handlers.AwsSdk;
 
-#elif  HOSTEDONPREMISES
+#elif HOSTEDONPREMISES
 using Infrastructure.Broker.RabbitMq.Configuration;
 using Infrastructure.Broker.RabbitMq.Publishing;
 using Infrastructure.Broker.RabbitMq.Topology;
 using Infrastructure.Broker.RabbitMq.Extensions;
 using Infrastructure.External.Persistence.OnPremises.ApplicationServices;
 using Microsoft.Extensions.Options;
-using Infrastructure.External.Persistence.OnPremises.ApplicationServices;
 
 #endif
 #endif
@@ -102,7 +101,9 @@ public static class HostExtensions
         RegisterPersistence(hostOptions.Persistence.UsesQueues, hostOptions.IsMultiTenanted);
         RegisterEventing(hostOptions.Persistence.UsesEventing);
         RegisterCors(hostOptions.CORS);
-
+#if !TESTINGONLY && HOSTEDONPREMISES
+        services.AddRabbitMqInfrastructure(appBuilder.Configuration);
+#endif
         var app = appBuilder.Build();
 
         // Note: The order of the middleware matters!
@@ -585,12 +586,12 @@ public static class HostExtensions
                 SqlServerBlobStore.Create(c.GetRequiredService<IRecorder>(),
                     SqlServerStoreOptions.Credentials(c.GetRequiredServiceForPlatform<IConfigurationSettings>())));
             services.AddForPlatform<IQueueStore>(c => RabbitMqQueueStore.Create(
-                    c.GetRequiredService<IMessagePublisher>(),
-                    c.GetRequiredService<ITopologyManager>(),
-                    c.GetRequiredService<IOptions<RabbitMqOptions>>().Value,
-                    c.GetRequiredService<IRecorder>(),
-                    c.GetRequiredService<ILoggerFactory>()
-                ));
+                c.GetRequiredService<IMessagePublisher>(),
+                c.GetRequiredService<ITopologyManager>(),
+                c.GetRequiredService<IOptions<RabbitMqOptions>>().Value,
+                c.GetRequiredService<IRecorder>(),
+                c.GetRequiredService<ILoggerFactory>()
+            ));
             services.AddForPlatform<IMessageBusStore>(c =>
                 RabbitMqMessageBusStore.Create(
                     c.GetRequiredService<IMessagePublisher>(),
@@ -617,7 +618,7 @@ public static class HostExtensions
                         c.GetRequiredService<IRecorder>(),
                         c.GetRequiredService<ILoggerFactory>()
                     ));
-        
+
                 // IMessageBusStore con RabbitMqMessageBusStore (Scoped)
                 services.AddPerHttpRequest<IMessageBusStore>(c =>
                     RabbitMqMessageBusStore.Create(
@@ -646,7 +647,7 @@ public static class HostExtensions
                         c.GetRequiredService<IRecorder>(),
                         c.GetRequiredService<ILoggerFactory>()
                     ));
-        
+
                 // IMessageBusStore con RabbitMqMessageBusStore (Singleton) - Esto debería ser seguro
                 services.AddSingleton<IMessageBusStore>(c =>
                     RabbitMqMessageBusStore.Create(
